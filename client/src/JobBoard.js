ce35870a73fcb00df0ae3e90b0de4fd0fb674e10
VladTzaru <bvanja.87@gmail.com>
2021-01-07 14:46:24 +0100

    Fetch jobs from client

--- client/src/JobBoard.js
+++ client/src/JobBoard.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { JobList } from './JobList';
-const { jobs } = require('./fake-data');
+import { fetchJobs } from './api/requests';
 
-export class JobBoard extends Component {
-  render() {
-    return (
-      <div>
-        <h1 className="title">Job Board</h1>
-        <JobList jobs={jobs} />
-      </div>
-    );
-  }
-}
+export const JobBoard = () => {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    const getJobs = async () => setJobs(await fetchJobs());
+    getJobs();
+  }, []);
+  return (
+    <div>
+      <h1 className='title'>Job Board</h1>
+      {jobs.length === 0 ? 'Loading...' : <JobList jobs={jobs} />}
+    </div>
+  );
+};
--- client/src/JobList.js
+++ client/src/JobList.js
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom';
 
 export class JobList extends Component {
   render() {
-    const {jobs} = this.props;
-    return (
-      <ul className="box">
-        {jobs.map(this.renderJob.bind(this))}
-      </ul>
-    );
+    const { jobs } = this.props;
+    return <ul className='box'>{jobs.map(this.renderJob.bind(this))}</ul>;
   }
 
   renderJob(job) {
-    const title = job.company ? `${job.title} at ${job.company.name}` : job.title;
+    const title = job.company
+      ? `${job.title} at ${job.company.name}`
+      : job.title;
     return (
-      <li className="media" key={job.id}>
-        <div className="media-content">
+      <li className='media' key={job.id}>
+        <div className='media-content'>
           <Link to={`/jobs/${job.id}`}>{title}</Link>
         </div>
       </li>
