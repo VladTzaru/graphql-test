ad92c4b884418242015fc3dcd49bf79f64174eeb
VladTzaru <bvanja.87@gmail.com>
2021-01-07 19:02:24 +0100

    Added company query and resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,8 +1,10 @@
 const db = require('./db');
 
 const Query = {
+  // First arg (root) is a parent and second is the query variables ({id})
   job: (_, { id }) => db.jobs.get(id),
   jobs: () => db.jobs.list(),
+  company: (_, { id }) => db.companies.get(id),
 };
 
 const Job = {
